e3cf558603cc1bfcf1e8dc82049c307f77c7c774
givoontake <givoontake@gmail.com>
2022-10-16 19:42:43 +0900

    파일 정리

---
+++ TermP.py
@@ -0,0 +1,12 @@
+from pico2d import *
+import game_framewark
+
+open_canvas()
+
+forest = load_image('forest.png')
+
+# open_canvas()
+# close_canvas()
+forest.draw_now(400, 300)
+# update_canvas()
+
