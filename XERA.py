4b4bbc1081ee6b32aff1d5b0d70ce61d090ba7b7
givoontake <givoontake@gmail.com>
2022-10-18 13:06:25 +0900

    캐릭터 좌우이동까지

--- XERA.py
+++ XERA.py
@@ -5,19 +5,19 @@ run_screen = True
 run_game = None
 run_shop = None
 
-screen = None
-forest = None
-shop = None
-LV = None
-
 s1 = 0
 s2 = 0
 s3 = 0
 s4 = 0
 s5 = 0
 
+x = 400
+frame = 0
+xdir = 0
+
 def handle_events():
-    global run_screen, run_game, run_shop, LV, s1, s2, s3, s4, s5
+    global run_screen, run_game, run_shop, LV, s1, s2, s3, s4, s5, xdir
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -37,6 +37,10 @@ def handle_events():
                 elif run_game == True:
                     run_game = False
                     run_shop = True
+            elif event.key == SDLK_d:
+                xdir += 1
+            elif event.key == SDLK_a:
+                xdir -= 1
             elif event.key == SDLK_1 and run_shop == True:
                 if s1 < 4:
                     s1 += 1
@@ -52,21 +56,33 @@ def handle_events():
             elif event.key == SDLK_5 and run_shop == True:
                 if s5 < 1:
                     s5 += 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_d:
+                    xdir -= 1
+            elif event.key == SDLK_a:
+                    xdir += 1
+def handle_events_character():
+    global run_game, xdir
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_d:
+                xdir += 1
+            elif event.key == SDLK_a:
+                xdir -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_d:
+                xdir -= 1
+            elif event.key == SDLK_a:
+                xdir += 1
 
 def mainscreen():
     global screen
     clear_canvas()
-    screen = load_image('mainscreen.png')
     screen.draw(400, 300)
-    update_canvas()
-    handle_events()
-
-
-def game():
-    global forest
-    clear_canvas()
-    forest = load_image('forest.png')
-    forest.draw(400, 350)
     update_canvas()
     handle_events()
 
@@ -74,8 +90,6 @@ def game():
 def upgrade_shop():
     global shop, LV, s1, s2, s3, s4, s5
     clear_canvas()
-    shop = load_image('shop.png')
-    LV = load_image('LV.png')
     shop.draw(430, 270)
     if s1 > -1:
         LV.clip_draw(31 * s1, 0, 35, 31, 725, 407)
@@ -91,7 +105,26 @@ def upgrade_shop():
     handle_events()
 
 
+def game():
+    global character, forest, x, xdir, frame
+    clear_canvas()
+    forest.draw(400, 350)
+    character.clip_draw(27 + frame * 42, 300, 42, 60, x, 90)
+    update_canvas()
+    handle_events()
+    # handle_events_character()
+    frame = (frame + 1) % 4
+    x += xdir * 5
+    delay(0.1)
+
+
 open_canvas()
+
+character = load_image('player.png')
+forest = load_image('forest.png')
+screen = load_image('mainscreen.png')
+shop = load_image('shop.png')
+LV = load_image('LV.png')
 
 while running:
     if run_screen == True:
--- test.py
+++ test.py
@@ -1,23 +1,56 @@
 from pico2d import *
 
-open_canvas()
+def handle_events_character():
+    global run_game, xdir
+    global running
+    global dir
 
-image = load_image('shop.png')
-image2 = load_image('LV.png')
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_d:
+                xdir += 1
+            elif event.key == SDLK_a:
+                xdir -= 1
+        elif event.key == SDLK_ESCAPE:
+            running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_d:
+                xdir -= 1
+            elif event.key == SDLK_a:
+                xdir += 1
 
 def test():
-    global image, image2
+    global image
     clear_canvas()
-    image.draw(430, 270)
-    # for i in range(10):
-    #     image2.clip_draw(31*i, 0, 35, 31, 725, 320)
-    #     update_canvas()
-    #     delay(1)
-    image2.clip_draw(31, 0, 35, 31, 725, 233)
-    update_canvas()
-# 407, 320, 233
+    image.draw(400, 350)
 
-test()
+
+def player():
+    while running:
+        global cha, image, x, xdir, frame
+        clear_canvas()
+        image.draw(400, 350)
+        cha.clip_draw(27+frame * 42, 300, 42, 60, x, 90)
+        update_canvas()
+        handle_events_character()
+        frame = (frame + 1) % 4
+        x += xdir * 5
+        delay(0.1)
+
+running = True
+x = 400
+frame = 0
+xdir = 0
+
+open_canvas()
+
+image = load_image('forest.png')
+cha = load_image('player.png')
+
+player()
 delay(3)
 
 close_canvas()
