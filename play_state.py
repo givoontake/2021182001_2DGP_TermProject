207f779dd9eda4bc907926388f01053518935175
givoontake <givoontake@gmail.com>
2022-11-11 17:06:07 +0900

    총알 발사 소리 구현

--- play_state.py
+++ play_state.py
@@ -1,5 +1,4 @@
 from pico2d import *
-# import winsound
 import game_framework
 import item_state
 
@@ -17,33 +16,39 @@ class Player:
         self.x, self.y = 400, 90
         self.frame = 0
         self.frame2 = 0
+        self.div = 0
         self.dir = 0
+        self.non_zero_dir = 1
         self.fire = False
         self.image = load_image('Player.png')
         self.item = None
 
     def update(self):
-        self.frame = (self.frame + 1) % 4
+        self.div += 1
+        if(self.div > 90):
+            self.div = 0
+        self.frame = self.div // 30
+        # self.frame = (self.frame + 1) % 4
         self.frame2 = (self.frame + 1) % 2
-        if hunter.fire == True:
-            self.bul += 1
-        else:
-            self.bul = 1
-        self.x += self.dir * 15
+        self.x += self.dir * 0.5
         if self.x > 750:
             self.x = 750
         elif self.x < 50:
             self.x = 50
 
     def draw(self):
-        # if self.dir > 0:
-        if hunter.fire == False:
-            self.image.clip_draw(40 + self.frame * 62, 450, 62, 90, self.x, 90)
-        else:
-            self.image.clip_draw(40, 320, 62, 90, self.x, 90)
-            self.image.clip_draw(320 + 60 * self.frame2, 225, 40, 80, self.x + 40, 90)
-        # elif self.dir < 0:
-        #     self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        if self.non_zero_dir > 0:
+            if hunter.fire == False:
+                self.image.clip_draw(40 + self.frame * 62, 450, 62, 90, self.x, 90)
+            else:
+                self.image.clip_draw(40, 320, 62, 90, self.x, 90)
+                self.image.clip_draw(320 + 60 * self.frame2, 225, 40, 80, self.x + 40, 90)
+        elif self.non_zero_dir < 0:
+            if hunter.fire == False:
+                self.image.clip_composite_draw(40 + self.frame * 62, 450, 62, 90, 0, 'flip', self.x, 90, 62, 90)
+            else:
+                self.image.clip_draw(40, 320, 62, 90, self.x, 90)
+                self.image.clip_draw(320 + 60 * self.frame2, 225, 40, 80, self.x + 40, 90)
 
 
 
@@ -52,11 +57,16 @@ class Bullet:
         self.image = load_image('Player.png')
         self.x = hunter.x
         self.y = hunter.y
-        self.bul = 0
+        self.fire_sound = load_wav('fire_sound.wav')
+        self.fire_sound.set_volume(10)
+        self.sound = True
+
+    def update(self):
+        # self.bul += 1
+        self.x += 5
 
     def draw(self):
-        self.image.clip_draw(320 + 60 * 2, 225, 20, 80, self.x + 40 + 40 * self.bul, 90)
-        self.bul += 1
+        self.image.clip_draw(320 + 60 * 2, 225, 20, 80, self.x + 40, 90)
 
 
 def handle_events():
@@ -70,8 +80,10 @@ def handle_events():
                 game_framework.push_state(item_state)
             elif event.key == SDLK_d:
                 hunter.dir = 1
+                hunter.non_zero_dir = 1
             elif event.key == SDLK_a:
                 hunter.dir = -1
+                hunter.non_zero_dir = -1
             elif event.key == SDLK_k:
                 bullets.append(Bullet())
                 hunter.dir = 0
@@ -98,25 +110,24 @@ running = True
 def bullet_draw():
     for bullet in bullets:
         bullet.draw()
+        if bullet.sound == True: # 처음 총알 객체 생성 때만 소리를 출력
+            bullet.fire_sound.play()
+            bullet.sound = False
 
 def bullet_del():
     for bullet in bullets:
-        print(bullet.x)
-        if bullet.x > 900:
-            bullets.remove(Bullet())
+        if bullet.x > 800 or bullet.x < 0:
+            bullets.remove(bullet)
 
-def check_delay():
-    if hunter.fire == False:
-        if len(bullets) > 0:
-            delay(0.01)
-        else:
-            delay(0.1)
+def bullet_update():
+    for bullet in bullets:
+        bullet.update()
 
 def enter():
     global hunter, forest, bullet, running
     hunter = Player()
     forest = Map()
-    bullet = Bullet
+    bullet = Bullet()
     running = True
 
 
@@ -132,15 +143,15 @@ def exit():
 # 게임 월드에 객체를 업데이트 - 게임 로직
 def update():
     hunter.update()
+    bullet_update()
     bullet_del()
 
 # 게임 월드 랜더링
 def draw_world():
-    check_delay()
     forest.draw()
     hunter.draw()
     bullet_draw()
-    print(len(bullets))
+    delay(0.001)
 
 def draw():
     clear_canvas()
